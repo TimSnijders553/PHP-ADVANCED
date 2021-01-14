698fd4994460fd495edb0e4747a72ec9c93f364f
Tim Snijders <tim.snijders@student.rocva.nl>
2021-01-14 11:59:55 +0100

    .

--- 2-Loops/taak04/index.php
+++ 2-Loops/taak04/index.php
@@ -6,6 +6,16 @@
 $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
 
 ?>
-
+<form method="post">
+<select name="Klasnaam">
+    <option value="">--Please choose an option--</option>
+    <?php
+    foreach($klassen as $klas) { ?>
+      <option value="<?php echo $klas; ?>">klas: <?php echo $klas; ?> </option>
+  <?php
+    } ?>
+</select>
+<input type="submit" value="knop">
+</form>
 </body>
 </html>
